c4b3ec06e625c10181e30848f464d83fdcac06cd
SoupLittle <marleneturlid@gmail.com>
2024-01-31 13:08:11 +0100

    *

--- index.js
+++ index.js
@@ -1,12 +1,11 @@
-var myCity = "Oslo"
-var myCountry = "Norway"
-var myKey = "09a03050c5fe26cbc384ac55c76cfe62"
-var myAPI = 'https://api.openweathermap.org/data/2.5/weather?q=${myCity}&units=metric&appid=${myKey}'
+let myCity = "Oslo"
+let myCountry = "Norway"
+let myKey = "09a03050c5fe26cbc384ac55c76cfe62"
+let myAPI = `https://api.openweathermap.org/data/2.5/weather?q=${myCity}&units=metric&appid=${myKey}`
 
 fetch(myAPI)
 .then (Response => Response.json())
 .then( data => {
-    console.log("Temperature: ", data.main.temp);
-    document.getElementById('city').innerHTML = `${data.name}, ${data.sys.country}`;
-    document.getElementById('temperature').innerHTML = `${Math.round(data.main.temp)} &#176;`;
+    console.log(data)
+    console.log(data.main.temp)
     })
