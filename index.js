37f1aa084afe1cbc9586e981c7111a4370176597
SoupLittle <marleneturlid@gmail.com>
2024-02-02 09:19:08 +0100

    *

--- index.js
+++ index.js
@@ -1,11 +1,33 @@
-let myCity = "Oslo"
-let myCountry = "Norway"
-let myKey = "09a03050c5fe26cbc384ac55c76cfe62"
-let myAPI = `https://api.openweathermap.org/data/2.5/weather?q=${myCity}&units=metric&appid=${myKey}`
 
-fetch(myAPI)
-.then (Response => Response.json())
-.then( data => {
-    console.log(data)
-    console.log(data.main.temp)
-    })
+
+function fetchData() {
+    let cityName = document.getElementById('cityName').value;
+    let apiURL = `https://api.openweathermap.org/data/2.5/weather?q=${cityName}&units=metric&appid=09a03050c5fe26cbc384ac55c76cfe62`;
+
+    fetch(apiURL)
+        .then(response => response.json())
+        .then(data => {
+            console.log(data);
+
+            document.getElementById('place').innerHTML = `<h3>${data.name}, ${data.sys.country}</h3>`;
+            document.getElementById('temp').innerHTML = `<p>${data.main.temp} &deg;C </p>`;
+            document.getElementById('skyies').innerHTML = `<img src="./CLoud.png" alt="A Single Cloud" height="60px" width="60px"><h5> Cloud coverage: ${data.clouds.all} %</h5>`;
+            document.getElementById('minTemp').innerHTML = `Min Temp: ${data.main.temp_min} &deg;C`;
+            document.getElementById('highTemp').innerHTML = `Max Temp: ${data.main.temp_max} &deg;C`;
+
+            // Skjuller elementene før man skriver sted
+            document.getElementById('skyies').style.display = 'block';
+            document.getElementById('minTemp').style.display = 'block';
+            document.getElementById('highTemp').style.display = 'block';
+        })
+    
+}
+
+document.getElementById('enterBtn').addEventListener('click', fetchData);
+
+// Gjør at man kan bruke enter tasten på keyboard
+document.getElementById('cityName').addEventListener('keypress', function (event) {
+    if (event.key === 'Enter') {
+        fetchData();
+    }
+});
